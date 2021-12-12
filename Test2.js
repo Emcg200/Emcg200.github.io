fbd57072c1344e24472cc66e34ebbcab544c7e1a
Ethan McGrath <ethanmcgrath@icloud.com>
2021-12-12 16:38:44 +0000

    JS Backend

---
+++ Test2.js
@@ -0,0 +1,23 @@
+function CreateHTMLProjectElements(){
+
+
+ // Project number: 0
+ //Element 0
+ // Text Component
+ var portolioElemet = document.getElementById("Test-1");
+ portolioElemet.innerHTML = "Test 1 Text";
+ //Element 1
+ // Link Component
+ var portolioElemet = document.getElementById("GitHubID");
+ portolioElemet.href = "https://emcg200.github.io/index.html";
+ //Element 2
+ // Link Component
+ var portolioElemet = document.getElementById("TwitterID");
+ portolioElemet.href = "https://twitter.com/Edev34";
+ //Element 3
+ // Link Component
+ var portolioElemet = document.getElementById("LinkedInID");
+ portolioElemet.href = "https://www.linkedin.com/in/ethan-mcgrath-464596198/";
+
+}
+
